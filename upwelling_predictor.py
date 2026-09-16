7a44056328810a073db67687efab0cf6e0e056aa
sw-of <sascha.winkler@thuenen.de>
2026-09-16 12:09:16 +0200

    Update upwelling_predictor.py

--- upwelling_predictor.py
+++ upwelling_predictor.py
@@ -380,7 +380,7 @@ def main():
         base_time_utc = get_next_raster_base_time(global_log_path)
         base_time_str = base_time_utc.strftime("%Y-%m-%d %H:%M")
         if base_time_str == last_base_time_str:
-            print(f"ℹ️ Reberechnung zu {last_base_str} bereits erfolgt. Beende aufholschleife.")
+            print(f"ℹ️ Reberechnung zu {base_time_str} bereits erfolgt. Beende aufholschleife.")
             break
         
         last_base_time_str = base_time_str
